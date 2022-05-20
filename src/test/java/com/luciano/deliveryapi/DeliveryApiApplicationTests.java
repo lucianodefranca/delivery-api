2200e5bca650b431debf0df448e42b8471e90e48
luciano de franca <lucianodefranca86@gmail.com>
2022-05-19 22:51:13 -0300

    first commit

---
+++ src/test/java/com/luciano/deliveryapi/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.deliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
