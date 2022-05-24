c5638115a3a0dd731181c66581e77c1fd92eff7e
luciano de franca <lucianodefranca86@gmail.com>
2022-05-23 23:33:32 -0300

    Cliente Repository

--- src/main/java/com/luciano/deliveryapi/controller/ClienteController.java
+++ src/main/java/com/luciano/deliveryapi/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package com.luciano.deliveryapi.controller;
 
 import com.luciano.deliveryapi.domain.model.Cliente;
+import com.luciano.deliveryapi.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,12 @@ import java.util.List;
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        return manager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
+       return clienteRepository.findAll();
     }
 
 }
---
+++ src/main/java/com/luciano/deliveryapi/domain/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.luciano.deliveryapi.domain.repository;
+
+import com.luciano.deliveryapi.domain.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
