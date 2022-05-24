1528c4c5b09b0d3037aea5c0e7ae8a1d9277ce41
luciano de franca <lucianodefranca86@gmail.com>
2022-05-23 23:26:15 -0300

    Jakarta EE

--- src/main/java/com/luciano/deliveryapi/controller/ClienteController.java
+++ src/main/java/com/luciano/deliveryapi/controller/ClienteController.java
@@ -4,28 +4,20 @@ import com.luciano.deliveryapi.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        var cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("João da Silva");
-        cliente1.setEmail("joao@email.com");
-        cliente1.setTelefone("41 9999-1234");
-
-        var cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Maria de Souza");
-        cliente2.setEmail("maria@email.com");
-        cliente2.setTelefone("41 8888-1234");
-
-        return Arrays.asList(cliente1, cliente2);
-
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 
 }
--- src/main/java/com/luciano/deliveryapi/domain/model/Cliente.java
+++ src/main/java/com/luciano/deliveryapi/domain/model/Cliente.java
@@ -3,12 +3,34 @@ package com.luciano.deliveryapi.domain.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String telefone;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
