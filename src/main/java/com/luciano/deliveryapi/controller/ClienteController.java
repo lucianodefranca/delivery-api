ba576523df039498396d875a0d0749568512af76
luciano de franca <lucianodefranca86@gmail.com>
2022-05-23 23:44:04 -0300

    Find by id cliente

--- src/main/java/com/luciano/deliveryapi/controller/ClienteController.java
+++ src/main/java/com/luciano/deliveryapi/controller/ClienteController.java
@@ -3,22 +3,30 @@ package com.luciano.deliveryapi.controller;
 import com.luciano.deliveryapi.domain.model.Cliente;
 import com.luciano.deliveryapi.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@RestController
+@RestController("/clientes")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar() {
        return clienteRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscaPorId(@PathVariable Long id) {
+        return clienteRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
